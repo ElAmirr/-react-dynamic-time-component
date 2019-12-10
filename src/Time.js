a9f40525ba2b747830ddbbbd43c61382f42ec128
ElAmirr <othmaniamir41gitgit config --global user.name ElAmirrgit config --global user.email othmaniamir41@gmail.com git config --global user.email othmaniamir41git>
2019-12-10 16:30:31 +0100

    first commit

---
+++ src/Time.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Time = () => 
+    <div className="time-container">
+        <div className="time-inner-container">
+            <div className="time-digits">
+                12:30:00
+            </div>
+            <div className="time-text">
+                <div className='time-text-item'>Hour</div>
+                <div className='time-text-item'>minute</div>
+                <div className='time-text-item'>second</div>
+            </div>
+        </div>
+    </div>
+
+export default Time
+
